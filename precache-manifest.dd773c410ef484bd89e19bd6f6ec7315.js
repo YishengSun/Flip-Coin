fd9dfa8bbba05e961daa79085772800e35fecce2
YishengSun <yisheng4@illinois.edu>
2020-01-14 21:21:17 -0600

    Updates

---
+++ precache-manifest.dd773c410ef484bd89e19bd6f6ec7315.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5297c51412beddd5ab3914ef09fc329e",
+    "url": "/flip-coin/index.html"
+  },
+  {
+    "revision": "34497353b864c223cecd",
+    "url": "/flip-coin/static/css/main.7b2a01c5.chunk.css"
+  },
+  {
+    "revision": "db212dec2bd1b559f05b",
+    "url": "/flip-coin/static/js/2.6d4c0d3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/flip-coin/static/js/2.6d4c0d3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34497353b864c223cecd",
+    "url": "/flip-coin/static/js/main.eb3b0e07.chunk.js"
+  },
+  {
+    "revision": "fcdbc075b82e7456b588",
+    "url": "/flip-coin/static/js/runtime-main.24fab9b0.js"
+  }
+]);
